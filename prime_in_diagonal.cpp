f2fbff5664ee33c622d0e8563fcb0eb76bbb0db5
Shiv_Prakash_Yadav <shiv979236@gmail.com>
2023-04-22 22:12:44 +0530

    leetcode_problems 2614

---
+++ prime_in_diagonal.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+   
+        bool isPrime(int n){
+        if(n<=1)
+          return false;
+      for(int i=2;i*i<=n;i++){
+        if(n%i==0)
+            return false;
+      }
+      return true;
+    }
+    int diagonalPrime(vector<vector<int>>& nums){
+      int n=nums.size(),ans=0;
+      for(int i=0;i<n;i++){
+        if(isPrime(nums[i][i]))
+            ans=max(ans,nums[i][i]);
+        if(isPrime(nums[i][n-i-1]))
+            ans=max(ans,nums[i][n-i-1]);
+      }
+      if(ans==0)
+          return 0;
+        else 
+            return ans;
+    }
+};
