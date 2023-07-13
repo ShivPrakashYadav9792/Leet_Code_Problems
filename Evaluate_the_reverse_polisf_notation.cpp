19409f09b2bfff84c67452546f2c0a33d73c6e9c
Shiv_Prakash_Yadav <shiv979236@gmail.com>
2023-07-13 11:03:30 +0530

    leetcode problems 150

---
+++ Evaluate_the_reverse_polisf_notation.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+       stack<int>s;
+        for( string it:tokens){
+           
+            if(it=="+"){
+                 int a=s.top();
+                s.pop();
+                int b=s.top();
+                 s.pop();
+                s.push(b+a);
+            }
+            else if(it=="-"){
+                 int a=s.top();
+            s.pop();
+            int b=s.top();
+            s.pop();
+                s.push(b-a);
+            }
+            else if(it=="*"){
+                 int a=s.top();
+            s.pop();
+            int b=s.top();
+            s.pop();
+                s.push(b*a);
+            }
+            else if(it=="/"){
+                 int a=s.top();
+            s.pop();
+            int b=s.top();
+            s.pop();
+                s.push(b/a);
+            }
+            else 
+                s.push(stoi(it));
+        }
+        return s.top();
+    }
+};
